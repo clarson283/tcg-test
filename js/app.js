04a78d6a4347f380148622df91ee08974fb0047b
Callie <clarson283@gmail.com>
2015-06-13 22:30:10 -0700

    more crap

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
-	
+
 function initialize() {
         var mapOptions = {
-          center: { lat: -34.397, lng: 150.644},
+          center: { lat: -3.070, lng: 37.349},
           zoom: 8
         };
         var map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
       }
+      /*var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+      var marker = new google.maps.Marker({
+			  position: myLatLng,
+			  map: map,
+			  icon: iconBase + '<span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>'
+		});*/
       google.maps.event.addDomListener(window, 'load', initialize);
 
 });
